6ba45c054d902130a18625109a4ddfe0ceea63b0
MDaffaIzzulhaqS <izzulhaqdaffasuyanto@gmail.com>
2023-04-20 14:58:04 +0700

    Initial Commit

---
+++ app/src/main/java/com/finalprojectkelompok6/calculatorapp/MainActivity.java
@@ -0,0 +1,42 @@
+package com.finalprojectkelompok6.calculatorapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.finalprojectkelompok6.calculatorapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.about) {
+                replaceFragment(new AboutFragment());
+            } else if (itemId == R.id.exit) {
+                System.exit(0);
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+}
