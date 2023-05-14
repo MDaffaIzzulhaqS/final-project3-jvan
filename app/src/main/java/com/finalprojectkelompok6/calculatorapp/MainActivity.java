4e8d23c21061f48666f125c3edce96ce41d2efad
Rendi Septia Rahman <rendiseptia287@gmail.com>
2023-05-14 18:22:10 +0700

    membuat aplikasi kalkulator

--- app/src/main/java/com/finalprojectkelompok6/calculatorapp/MainActivity.java
+++ app/src/main/java/com/finalprojectkelompok6/calculatorapp/MainActivity.java
@@ -4,96 +4,146 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.android.material.button.MaterialButton;
+public class MainActivity extends AppCompatActivity{
 
-import org.mozilla.javascript.Context;
-import org.mozilla.javascript.Scriptable;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
-    TextView resultTv,solutionTv;
-    MaterialButton buttonC,buttonBrackOpen,buttonBrackClose;
-    MaterialButton buttonDivide,buttonMultiply,buttonPlus,buttonMinus,buttonEquals;
-    MaterialButton button0,button1,button2,button3,button4,button5,button6,button7,button8,button9;
-    MaterialButton buttonAC,buttonDot;
-
+    private TextView inputText, outputText;
+    private String input, output;
+    private Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9,
+            btnTambah, btnKurang, btnKali, btnBagi, btnPersen, btnPangkat, btnTitik, btnSamaDengan,
+            btnClear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        resultTv = findViewById(R.id.result_tv);
-        solutionTv = findViewById(R.id.solution_tv);
 
-        assignId(buttonC,R.id.button_c);
-        assignId(buttonBrackOpen,R.id.button_open_bracket);
-        assignId(buttonBrackClose,R.id.button_close_bracket);
-        assignId(buttonDivide,R.id.button_divide);
-        assignId(buttonMultiply,R.id.button_multiply);
-        assignId(buttonPlus,R.id.button_plus);
-        assignId(buttonMinus,R.id.button_minus);
-        assignId(buttonEquals,R.id.button_equals);
-        assignId(button0,R.id.button_0);
-        assignId(button1,R.id.button_1);
-        assignId(button2,R.id.button_2);
-        assignId(button3,R.id.button_3);
-        assignId(button4,R.id.button_4);
-        assignId(button5,R.id.button_5);
-        assignId(button6,R.id.button_6);
-        assignId(button7,R.id.button_7);
-        assignId(button8,R.id.button_8);
-        assignId(button9,R.id.button_9);
-        assignId(buttonAC,R.id.button_ac);
-        assignId(buttonDot,R.id.button_dot);
+        inputText = findViewById(R.id.input_text);
+        outputText = findViewById(R.id.output_text);
 
+        btn0 = findViewById(R.id.btn_no0);
+        btn1 = findViewById(R.id.btn_no1);
+        btn2 = findViewById(R.id.btn_no2);
+        btn3 = findViewById(R.id.btn_no3);
+        btn4 = findViewById(R.id.btn_no4);
+        btn5 = findViewById(R.id.btn_no5);
+        btn6 = findViewById(R.id.btn_no6);
+        btn7 = findViewById(R.id.btn_no7);
+        btn8 = findViewById(R.id.btn_no8);
+        btn9 = findViewById(R.id.btn_no9);
+
+        btnTambah = findViewById(R.id.btn_tambah);
+        btnKurang = findViewById(R.id.btn_kurang);
+        btnKali = findViewById(R.id.btn_kali);
+        btnBagi = findViewById(R.id.btn_bagi);
+        btnPersen = findViewById(R.id.btn_persen);
+        btnPangkat = findViewById(R.id.btn_pangkat);
+        btnTitik = findViewById(R.id.btn_titik);
+        btnSamaDengan = findViewById(R.id.btn_sama_dengan);
+        btnClear = findViewById(R.id.btn_clear);
     }
 
-    void assignId(MaterialButton btn,int id){
-        btn = findViewById(id);
-        btn.setOnClickListener(this);
+    public void onButtonClicked(View view) {
+        Button button = (Button) view;
+        String data = button.getText().toString();
+
+        switch (data) {
+            case "C":
+                input = null;
+                output = null;
+                outputText.setText("");
+                break;
+            case "^":
+                solve();
+                input += "^";
+                break;
+            case "*":
+                solve();
+                input += "*";
+                break;
+            case "%":
+                input += "%";
+                double d = Double.parseDouble(inputText.getText().toString()) / 100;
+                outputText.setText(String.valueOf(d));
+                break;
+            case "=":
+                solve();
+                break;
+            default:
+                if (input == null) {
+                    input = "";
+                } else if (data.equals("+") || data.equals("-") || data.equals("/")) {
+                    solve();
+                }
+                input += data;
+        }
+        inputText.setText(input);
     }
 
-
-    @Override
-    public void onClick(View view) {
-        MaterialButton button =(MaterialButton) view;
-        String buttonText = button.getText().toString();
-        String dataToCalculate = solutionTv.getText().toString();
-
-        if(buttonText.equals("AC")){
-            solutionTv.setText("");
-            resultTv.setText("0");
-            return;
+    private void solve() {
+        if (input.split("\\+").length == 2) {
+            String numbers [] = input.split("\\+");
+            try {
+                double d = Double.parseDouble(numbers[0]) + Double.parseDouble(numbers[1]);
+                output = Double.toString(d);
+                outputText.setText(output);
+                input = d + "";
+            } catch (Exception e) {
+                outputText.setError(e.getMessage().toString());
+            }
         }
-        if(buttonText.equals("=")){
-            solutionTv.setText(resultTv.getText());
-            return;
+        if (input.split("\\*").length == 2) {
+            String numbers [] = input.split("\\*");
+            try {
+                double d = Double.parseDouble(numbers[0]) * Double.parseDouble(numbers[1]);
+                output = Double.toString(d);
+                outputText.setText(output);
+                input = d + "";
+            } catch (Exception e) {
+                outputText.setError(e.getMessage().toString());
+            }
         }
-        if(buttonText.equals("C")){
-            dataToCalculate = dataToCalculate.substring(0,dataToCalculate.length()-1);
-        }else{
-            dataToCalculate = dataToCalculate+buttonText;
+        if (input.split("\\/").length == 2) {
+            String numbers [] = input.split("\\/");
+            try {
+                double d = Double.parseDouble(numbers[0]) / Double.parseDouble(numbers[1]);
+                output = Double.toString(d);
+                outputText.setText(output);
+                input = d + "";
+            } catch (Exception e) {
+                outputText.setError(e.getMessage().toString());
+            }
         }
-        solutionTv.setText(dataToCalculate);
-
-        String finalResult = getResult(dataToCalculate);
-
-        if(!finalResult.equals("Err")){
-            resultTv.setText(finalResult);
+        if (input.split("\\^").length == 2) {
+            String numbers [] = input.split("\\^");
+            try {
+                double d = Math.pow(Double.parseDouble(numbers[0]), Double.parseDouble(numbers[1]));
+                output = Double.toString(d);
+                outputText.setText(output);
+                input = d + "";
+            } catch (Exception e) {
+                outputText.setError(e.getMessage().toString());
+            }
         }
-    }
-
-    String getResult(String data){
-        try{
-            Context context  = Context.enter();
-            context.setOptimizationLevel(-1);
-            Scriptable scriptable = context.initStandardObjects();
-            String finalResult =  context.evaluateString(scriptable,data,"Javascript",1,null).toString();
-            return finalResult;
-        }catch (Exception e){
-            return "Err";
+        if (input.split("\\-").length == 2) {
+            String numbers [] = input.split("\\-");
+            try {
+                if (Double.parseDouble(numbers[0]) < Double.parseDouble(numbers[1])) {
+                    double d = Double.parseDouble(numbers[1]) - Double.parseDouble(numbers[0]);
+                    output = Double.toString(d);
+                    outputText.setText("-" + output);
+                    input = d + "";
+                } else {
+                    double d = Double.parseDouble(numbers[0]) - Double.parseDouble(numbers[1]);
+                    output = Double.toString(d);
+                    outputText.setText(output);
+                    input = d + "";
+                }
+            } catch (Exception e) {
+                outputText.setError(e.getMessage().toString());
+            }
         }
     }
 }
